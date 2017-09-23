62d87b8859de6e108a450ab58867737ebecee943
Caleb Hayashida <santeyio@gmail.com>
2017-09-23 02:22:31 -0400

    edit manifest paths

--- sw.js
+++ sw.js
@@ -19,23 +19,19 @@ importScripts('workbox-sw.prod.v2.0.1.js');
  */
 const fileManifest = [
   {
-    "url": "/index.html",
-    "revision": "d0236c938b81439895791b227e675c6f"
-  },
-  {
-    "url": "/static/css/app.a2a6686344dc798b4d955edd25b459cf.css",
+    "url": "dist/static/css/app.a2a6686344dc798b4d955edd25b459cf.css",
     "revision": "ed84fcc9e0e351d0929a9e216828c066"
   },
   {
-    "url": "/static/js/app.fbf78670ec8ef2480de5.js",
+    "url": "dist/static/js/app.fbf78670ec8ef2480de5.js",
     "revision": "af24be0b250e31676c0dff6f6c203499"
   },
   {
-    "url": "/static/js/manifest.77bd5bf18904d80e389e.js",
+    "url": "dist/static/js/manifest.77bd5bf18904d80e389e.js",
     "revision": "1136b8eddce9a9d7fd4ce0f0d215ae49"
   },
   {
-    "url": "/static/js/vendor.448f2d15e3f6731dd22b.js",
+    "url": "dist/static/js/vendor.448f2d15e3f6731dd22b.js",
     "revision": "2109a61b8fcb286b07346860baf9aca3"
   }
 ];
